f62a0befabeecd575ccecb9adb292f5cc02d5a18
unknown <ryandufault1219@gmail.com>
2025-09-25 19:59:53 -0400

    + Header fait
    Quelques trucs à fix :
    - La taille invisible qu'occupe le deuxième svg dans l'espace
    - Trouver une façon de lisser l'animation du 2ème svg

---
+++ scripts.js
@@ -0,0 +1,38 @@
+gsap.registerPlugin(ScrollTrigger);
+
+/* v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v= */
+/* ANIM HEADER     ANIM HEADER     ANIM HEADER        */
+/* v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v=v= */
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".responsive-svg",
+    start: "top", // initialisation timeline, start: top, end : 1 vh de + que la fin du viewheight de début
+    end: "bottom+=100%",
+    scrub: 3,
+  },
+});
+
+tl.to([".responsive-svg:nth-child(1)", ".header-links"], {
+  y: "-100vh",
+  duration: 3,
+  ease: "none",
+})
+  .set(".responsive-svg:nth-child(1)", {
+    display: "none", // fait disparaitre le 1er svg
+  })
+  .set(".responsive-svg:nth-child(2)", {
+    display: "block",
+    opacity: 1, // fait apparaitre le 2ème svg
+  })
+  .to(".responsive-svg:nth-child(2)", {
+    y: 0,
+    ease: "none",
+  })
+  .to(".header-links", {
+    display: "none", // fait disapraitre les liens
+  });
+
+/* ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^= */
+/* ANIM HEADER     ANIM HEADER     ANIM HEADER        */
+/* ^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^=^= */
