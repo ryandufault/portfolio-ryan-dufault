810270baa3f52b38749c041e762be5427a3a3955
unknown <ryandufault1219@gmail.com>
2025-09-25 20:21:50 -0400

    + Header finit
    - Changement d'animation GSAP

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ let tl = gsap.timeline({
     trigger: ".responsive-svg",
     start: "top", // initialisation timeline, start: top, end : 1 vh de + que la fin du viewheight de début
     end: "bottom+=100%",
-    scrub: 3,
+    scrub: 2,
   },
 });
 
@@ -26,7 +26,7 @@ tl.to([".responsive-svg:nth-child(1)", ".header-links"], {
     opacity: 1, // fait apparaitre le 2ème svg
   })
   .to(".responsive-svg:nth-child(2)", {
-    y: 0,
+    clipPath: "inset(0% 0% 0% 0%)", // fait apparaitre le 2ème svg par le bas avec un mask
     ease: "none",
   })
   .to(".header-links", {
